9173749a13682d0ac6363f9a3af06c3785fb5bc1
ockwoo <hyunwoo.ock@gmail.com>
2016-06-16 00:08:18 +0900

    modified for log

--- server/lettle/config.js
+++ server/lettle/config.js
@@ -1,4 +1,11 @@
-//'mongodb' : 'mongodb://mustmakeitadmin:mustMake!q2w#e@localhost:27017'
-module.exports = {
-    'mongodb' : 'mongodb://localhost:27017'
+var PRD = {
+	'mongodb' : 'mongodb://mustmakeitadmin:mustMake!q2w#e@localhost:27017',
+    'logLevel' : 'debug'
 };
+
+var DEV = {
+	'mongodb' : 'mongodb://localhost:27017',
+    'logLevel' : 'debug'
+};
+
+module.exports = DEV;
